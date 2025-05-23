0bdb1a13fce40fd94d9cb79fdc1ad78510df761a
Israel Ramirez Morales <israelramirezp1@gmail.com>
2025-05-23 08:18:02 -0600

    Ojala funcione

--- hola.java
+++ hola.java
@@ -2,7 +2,7 @@ public class hola {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Gabo is GAY");
-            System.out.println(5+3);
+        System.out.println(5+3);
     }
 }
 
